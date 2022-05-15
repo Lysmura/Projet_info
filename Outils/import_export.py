77cea9c6dd24340d194e9af52574a7aa41414e7d
meda6li <105364732+meda6li@users.noreply.github.com>
2022-05-16 01:23:23 +0200

    Update import_export.py

--- Outils/import_export.py
+++ Outils/import_export.py
@@ -1,5 +1,15 @@
 class Import_export:
     def __init__(self, chemin , nom_fichier):
-        self.chemin = chemin
+        self.chemin = Import_export.fix_chemin(chemin)
         self.nom_fichier = nom_fichier
+
+    @staticmethod
+    def fix_chemin(chemin): 
+        nouveau_chemin=''
+        for caractere in chemin:
+            if caractere == "\\":
+                nouveau_chemin += "/"
+            else:
+                nouveau_chemin += caractere
+        return nouveau_chemin
     
