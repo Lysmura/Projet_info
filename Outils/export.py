2183230c671e5ea6223b08a8fde1b2fd803f4ef7
meda6li <105364732+meda6li@users.noreply.github.com>
2022-05-16 01:27:53 +0200

    Update export.py

--- Outils/export.py
+++ Outils/export.py
@@ -1,16 +1,17 @@
 import csv
 from my_export import export
+import Dataframe
 
 class Export(export):
-    def __init__(self, header, separator, chemin, nom_fichier  ):
-        self.header = header
+    def __init__(self, separator : str , chemin : str, nom_fichier  ):
         self.separator = separator
         super().__init__(chemin, nom_fichier)
         
-    def exporting(self, dataframe):
-        with open(self.nom_fichier, 'w', newline='') as f:
-            writer = csv.writer(f, delimiter = self.separator)
-            writer.writerow(self.header)
-            writer.writerows(dataframe)
+    def exporting(self, dataframe : Dataframe):
+        with open(self.nom_fichier, mode = 'w', newline='') as csv_file:
+            csv_file.write(self.separator.join([dataframe.header]))
+            for i in range (0, dataframe.__len__()):
+                Ligne = self.separator.join(dataframe.ligne(i)) + "\n"
+                csv_file.write(Ligne)
         
 
