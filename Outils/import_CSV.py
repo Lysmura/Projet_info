53457bd58419cb6065f9ae710d48d99506efce9d
meda6li <105364732+meda6li@users.noreply.github.com>
2022-05-16 01:22:39 +0200

    Update import_CSV.py

--- Outils/import_CSV.py
+++ Outils/import_CSV.py
@@ -1,19 +1,29 @@
 import gzip
 import csv
 from my_import import Import
+
 class import_csv(Import):
-    def __init__(self,chemin,nom_fichier):
+    def __init__(self,chemin, nom_fichier):
         super().__init__(chemin, nom_fichier)
+
     
     def importing(self) :
         data=[]
-        with gzip.open(self.chemin +'/' + self.nom_fichier, mode='rt') as gzfile :
+        with gzip.open(self.chemin + '/' + self.nom_fichier, mode='rt') as gzfile :
             synopreader = csv.reader(gzfile, delimiter=';')
             for row in synopreader :
                 data.append(row)
-        return data
+        
+        cles=[str(k) for k in range(0,len(data)-1)]
+        lignes = data[1:]
+        new_data = dict()
+        
+        for key, value in zip(cles, lignes):
+            new_data[key] = value
+        
+        return new_data
 
-data_example = import_csv('../donnees_meteo/donnees_meteo','synop.201301.csv.gz').importing()
-print(data_example)
+Donnée_exemple = import_csv('D:\Projet_info\donnee\synop.csv.gz-20220511\donnees_meteo','synop.201301.csv.gz').importing()
+print(Donnée_exemple)
     
 
