539ebf0123d7f3dba4e39ad9b461bd9acae69068
Mohamed-Assili <105364732+meda6li@users.noreply.github.com>
2022-05-16 20:23:42 +0100

    refactoring du code csv

--- Outils/import_CSV.py
+++ Outils/import_CSV.py
@@ -6,24 +6,27 @@ class import_csv(Import):
     def __init__(self,chemin, nom_fichier):
         super().__init__(chemin, nom_fichier)
 
-    
-    def importing(self) :
+    def dataOutput(self) :
         data=[]
         with gzip.open(self.chemin + '/' + self.nom_fichier, mode='rt') as gzfile :
             synopreader = csv.reader(gzfile, delimiter=';')
             for row in synopreader :
                 data.append(row)
-        
-        cles=[str(k) for k in range(0,len(data)-1)]
-        lignes = data[1:]
-        new_data = dict()
-        
-        for key, value in zip(cles, lignes):
-            new_data[key] = value
-        
-        return new_data
+        return data
 
-Donnée_exemple = import_csv('D:\Projet_info\donnee\synop.csv.gz-20220511\donnees_meteo','synop.201301.csv.gz').importing()
-print(Donnée_exemple)
+    def importing(self):
+        data = self.dataOutput()  
+        header = data[0]
+
+        new_data ={}
+        for index, value in enumerate(data[1:]):
+            new_data[index]=value
+
+        return (header, new_data)
+
+header, data = import_csv('../synop.csv.gz-20220511/donnees_meteo','synop.201301.csv.gz').importing()
+
+print(data)
+print(header)
     
 
