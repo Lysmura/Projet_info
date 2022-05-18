a34a8197701cc953ede766ab25f0723e3fd3bff5
Lysmura <lysmura@gmail.com>
2022-05-18 23:07:42 +0200

    maj

--- Statistiques/Max.py
+++ Statistiques/Max.py
@@ -1,19 +1,21 @@
 import Statistiques.Univaries as Univaries
+import unittest
 
 class Max(Univaries):
     """
     Calcule la valeur maximum d'une colonne choisit
 
     Methods:
+    --------
     _operation(col) : var
         Retourne la valeur maximum d'une variable numérique dans une colonne
+
+    Exemples:
+    ---------
     """
     def _operation(self, col):
-        try:
-            if isinstance(col[0], int) == False and isinstance(col[0], float) == False :
+        if isinstance(col[0], int) is False and isinstance(col[0], float) is False :
                 raise ValueError("Le format de la variable n'est pas bon")
-        except ValueError as ve:
-            print(ve)
         if len(col) == 0:
             return None
         else :
--- Structure/__main__.py
+++ Structure/__main__.py
@@ -1,7 +1,13 @@
-from Dataframe import Dataframe as df
+from Dataframe import *
 
 header=[["Race", "str"]]
-data={0:"Matin", 1:"Jack Russel", 2:"Corgi"}
-donnees = Dataframe("chien", header, data)
+data={0:["Matin"], 1:["Jack Russel"], 2:["Corgi"]}
+chiens = Dataframe("chien", header, data)
 
-donnees.df.col("poids", [12.4, 4.1, 5.5])
+chiens.add_col("poids", [22.4, 4.1, 5.5])
+chiens.add_ligne(["Levrier Afghan", 17])
+# chiens.add_ligne([13, "Bulldog anglais"])
+#chiens.del_col("poids")
+print(chiens.data[2])
+#print(chiens)
+#print(chiens.header)
