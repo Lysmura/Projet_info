e2d2088aae7577ed13f5e68d6b05507aa3ec05cc
meda6li <105364732+meda6li@users.noreply.github.com>
2022-05-21 13:47:10 +0200

    Update my_export.py

--- Outils/my_export.py
+++ Outils/my_export.py
@@ -1,13 +1,14 @@
 from abc import ABC ,abstractmethod
-from Outils.import_export import Import_export
+from Outils.import_export import import_export
 
-class export(Import_export, ABC):
+class my_export(import_export, ABC):
     def __init__(self, chemin, nom_fichier):
         super().__init__(chemin, nom_fichier)
     
     @abstractmethod
-    def exporting(self, dataframe):
+    def exporting(self):
         '''
         '''
         pass
+
 
