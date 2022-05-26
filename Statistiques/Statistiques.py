79eaf88aa2bce98f31dddc133b6f96d4fd9264e7
meda6li <105364732+meda6li@users.noreply.github.com>
2022-05-26 12:56:32 +0200

    Update Statistiques.py

--- Statistiques/Statistiques.py
+++ Statistiques/Statistiques.py
@@ -6,19 +6,8 @@ import Structure.Dataframe as df
 from abc import ABC, abstractmethod
 
 class Statistiques(ABC) :
-    def __init__(self, X, Y):
-        self.X = X 
-        self.Y = Y
-        self.Results = stats.linregress(x=self.X, y=self.Y)
-
-
-    def beta(self):
-        return self.Results.slope 
-    
-    def beta0(self):
-        return self.Results.intercept
-
-    def Err(self):
-        return self.Results.stderr
+    @abstractmethod
+    def __init__(self):
+        pass
 
 
