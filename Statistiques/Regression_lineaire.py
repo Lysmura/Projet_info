03553ee81c6868af837c8ae786ac7970bdf4c0dd
meda6li <105364732+meda6li@users.noreply.github.com>
2022-05-27 22:32:27 +0200

    Update Regression_lineaire.py

--- Statistiques/Regression_lineaire.py
+++ Statistiques/Regression_lineaire.py
@@ -1,15 +1,19 @@
-from bivaries import Bivaries
+from Bivaries import Bivaries
+from scipy import stats 
+import matplotlib.pyplot as plt
 
 class Regression_lineaire(Bivaries):
+
     def __init__(self, col_x, col_y):
         super.__init__(col_x, col_y)
         self.Results = stats.linregress(x=self.col_x, y=self.col_y)
+        self.beta0 = self.Results.intercept
+        self.beta1 = self.Results.slope
+        self.erreur = self.Results.stderr
 
-    def beta(self):
-        return self.Results.slope 
-    
-    def beta0(self):
-        return self.Results.intercept
 
-    def Err(self):
-        return self.Results.stderr
+    def _operation(self):
+        plt.plot(self.col_x, self.col_y, 'o', label='Data')
+        plt.plot(self.col_x, self.beta0 + self.beta1*self.col_x, 'r', label='Droite de regression')
+        plt.legend()
+        plt.show()
