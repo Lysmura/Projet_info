05e464a841ddf6fd5fe192762d764cd853b29f60
Lysmura <lysmura@gmail.com>
2022-05-28 22:23:17 +0200

    maj

--- Structure/__main__.py
+++ Structure/__main__.py
@@ -13,13 +13,6 @@ chiens2 = Dataframe("chiens",
                                 2:["Corgi", 10.1],
                                 3:["mq", 20]})
 
-chiens3 = Dataframe("chiens",
-                                [["Race", "str"], ["Poids", "float"]],
-                                {0:["Matin", 60],
-                                1:["Jack Russel", 7],
-                                2:["Corgi", 10.1],
-                                3:["mq", 20],
-                                4:["Levrier Afghan", 27]})
 chiens1.add_col("Poids", [60.0, 7, 10.1, 20])
 print(chiens1)                      
 print(chiens2)
