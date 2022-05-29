3ba52b40e782c3360be01e7ce32374ac83fadbe6
meda6li <mohamedassili2@gmail.com>
2022-05-29 15:33:03 +0200

    Update test_moyenne_glissante.py

--- Transformation/test/test_moyenne_glissante.py
+++ Transformation/test/test_moyenne_glissante.py
@@ -6,7 +6,7 @@ import unittest
 class Test_moyenne_glissante(unittest.TestCase):
     header, data = import_json('Data/electricity_data','2013-01.json.gz').importing()
     data_frame =Dataframe(nom="donneeselectr", header = header, data = data)
-    moy_gli = moyenne_glissante(data_frame,['consommation_brute_gaz_grtgaz'])._operation(266)[0]
+    moy_gli = moyenne_glissante(data_frame,['consommation_brute_gaz_grtgaz'])._operation(4)[0]
     print(moy_gli)
 
 if __name__ == '__main__':
