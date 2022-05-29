3ebe6550c2d8805aa3dc41ba09ca9da88d9d8a31
meda6li <mohamedassili2@gmail.com>
2022-05-29 20:11:43 +0200

    Update transformation_moyenne_glissante.py

--- Transformation/transformation_moyenne_glissante.py
+++ Transformation/transformation_moyenne_glissante.py
@@ -5,13 +5,15 @@ from Structure.dataframe import Dataframe
 class moyenne_glissante(Transformation):
     def __init__(self, df_1 : Dataframe, var):
         super().__init__(df_1,var)
-    
-    def _operation(self, pas : int):    
-        L=[]
+
+    def _operation(self, pas : int):
+        l_moyenne_glissante=[]
         col = self.df_1.col(self.var[0])
         for i in range(len(col)):
-            try:
-                L += [Moyenne(col[i-pas:i+1])._operation()]
-            except IndexError:
-                pass
-        return L
+            if i+1-pas<0 :
+                l_moyenne_glissante.append('mq')
+            else :
+                temp = [col[elem] for elem in range(i+1-pas , i+1)]
+                temp = Moyenne(temp)._operation()
+                l_moyenne_glissante.append(temp)
+        return l_moyenne_glissante
