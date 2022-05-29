280131a0bcbe090122e5a790cbd25a1b7847111d
ayoub1brm <105363414+ayoub1brm@users.noreply.github.com>
2022-05-29 23:48:03 +0200

    Update pipeline_test_a_supp.py

--- Structure/pipeline_test_a_supp.py
+++ Structure/pipeline_test_a_supp.py
@@ -4,74 +4,10 @@ from Statistiques.max import Max
 from Statistiques.min import Min
 from Statistiques.ecart_type import Ecart_type
 from Statistiques.compter import Compter
-
-class Pipeline(self,liste_operations):
-    """
-    Classe définissant un pipeline, son but est d'appliquer une série de fonctions
-
-    Attributes
-    ----------
-    self.__liste_operations : list
-        Liste d'opération et des arguments à appliquer sur le pipeline
-
-    Methods
-    -------
-    __init__(__liste_operations)
-        Createur du pipeline
-    ajouter_operation(nom : str, arugments : list)
-        Ajoute une nouvelle opération à appliquer au dataframe
-    supprimer_operation(numero:int)
-        Remove operations to the Pipeline after it's creation
-    operations : list
-        Retourne une liste des opérations disponibles
-    execution(df) : DataFrame
-        Execute le pipeline sur un dataframe d'origine
-    """
-
-    def __init__(self, __liste_operations):
-        self.__liste_operations = []
-
-    def add_operation(self,nom, arguments):
-        """
-        """
-        if nom in ["Statistiques", "Compter"]:
-            if isinstance(arguments, str) :
-                print("L'argument doit être le nom d'une colonne")
-                raise TypeError
-            else:
-                self.__liste_operations.append([nom,arguments])
-        if nom == "Regression linéaire":
-            if (isinstance(arguments, list) and len(arguments)==2 
-                and isinstance(arguments[0], str) and isinstance(arguments[1], str)) :
-                print("L'argument doit être une liste avec le nom de deux colonnes")
-                raise TypeError
-            else:
-                self.__liste_operations.append([nom,arguments])
-       # if : #à poursuivre
-
-
-    #def supprimer_operation(self, numero):
-        #h
-        
-
-    def execution(self, df : Dataframe):
-        for operation in self.__liste_operations:
-            nom = operation[0]
-            arguments = operation[1]
-            if nom == "Statistiques" :
-                col = df.col(arguments)
-                print({"Moyenne" : Moyenne(col)._operation(),
-                        "Max" :  Max(col)._operation(),
-                        "Min" : Min(col)._operation(),
-                        "Ecart type" : Ecart_type(col)._operation()})
-            if nom == "Compter":
-                freq = False
-                freq = input("Tapez True pour obtenir les fréquences, sinon faites entrée")
-                col = df.col(arguments)
-                print(Compter(col, freq)._operation())
-            if nom == "Regression linéaire" :
-                
-                
+from Outils.my_import import my_import
+from Outils.my_export import my_export
+from Transformation.transformation_group_by import Groupby
+from Transformation.transformation_join import Join
 from Transformation.transformation_transformation import Transformation
 from Statistiques.univaries import Univaries
 from Statistiques.Bivaries import Bivaries
@@ -85,7 +21,7 @@ class Pipeline:
         self.__op_possible = [Transformation,Univaries,Bivaries,import_export]
     
     def ajouter_operation(self,operation):
-        validation_operation = [isinstance(operation,op_possible) for op_possible in self.__op_possible]
+        validation_operation = [isinstance(operation[1],op_possible) for op_possible in self.__op_possible]
         if any(validation_operation):
             self.__liste_operations.append(operation)
         else:
@@ -96,8 +32,115 @@ class Pipeline:
             self.__liste_operations.pop(numero)
 
     def execution(self):
-        trans_df = Dataframe('Resultat',[],{})
+        stockage_df = []
         dico_stat = {}
         for i in range(len(self.__liste_operations)):
             if isinstance(self.__liste_operations[i],Univaries):
-                dico_stat.update({i : self.__liste_operations[i]._operation()})
+                try:
+                    if len(stockage_df) == 0:
+                        raise ValueError('vous n\'avez importer ou stocker aucun dataframe pour travailler dessus')
+                except ValueError as ve:
+                    print(ve)
+                    return ve
+                try:
+                    if not isinstance(self.__liste_operations[i][0],Dataframe):
+                        raise ValueError("pour une statistique descriptîve vous devez indiqué le dataframe en première position")
+                except ValueError as ve:
+                    print(ve)
+                    return ve
+                try:
+                    if self.__liste_operations[i][0] not in stockage_df:
+                        raise IndexError('le dataframe renseigné n\'existe pas')
+                except IndexError as ie:
+                    print(ie)
+                    return ie
+                try:
+                    if all([self.__liste_operations[i][1].col != nom_colone[0] for nom_colone in self.__liste_operations[i][0].header]):
+                        raise IndexError("la variable {} est mal orthographié ou n'appartient pas aux dataframes indiqués".format(nom_var))
+                except IndexError as ie:
+                    print(ie)
+                    return ie
+                if  (self.__liste_operations[i-1][0] and isinstance(self.__liste_operations[i-1][2],Groupby)):
+                    for table in stockage_df[-1]:
+                        self.__liste_operations[i][1].col = table.col(self.__liste_operations[i][1].col)
+                        print('stat pour' +table.nom)
+                        print(self.__liste_operations[i][1].__operation())
+                        dico_stat.update({i:self.__liste_operations[i][1].__operation()})   
+                else:
+                    self.__liste_operations[i][1].col = self.__liste_operations[i][0].col(self.__liste_operations[i][1].col)
+                    print(self.__liste_operations[i][1].__operation())
+                    dico_stat.update({i:self.__liste_operations[i][1].__operation()})
+            
+            if isinstance(self.__liste_operations[i],Bivaries):
+                try:
+                    if len(stockage_df) == 0:
+                        raise ValueError('vous n\'avez importer ou stocker aucun dataframe pour travailler dessus')
+                except ValueError as ve:
+                    print(ve)
+                    return ve
+                try:
+                    if not isinstance(self.__liste_operations[i][0],Dataframe):
+                        raise ValueError("pour une statistique descriptîve vous devez indiqué le dataframe en première position")
+                except ValueError as ve:
+                    return ve
+                try:
+                    if all([self.__liste_operations[i][1].col_x != nom_colone[0] for nom_colone in self.__liste_operations[i][0].header]) or all([self.__liste_operations[i][1].col_y != nom_colone[0] for nom_colone in self.__liste_operations[i][0].header]):
+                        raise IndexError("une des variables variable est mal orthographié ou n'appartient pas aux dataframes indiqués")
+                except IndexError as ie:
+                    return ie
+                if (self.__liste_operations[i-1][0] and isinstance(self.__liste_operations[i-1][2],Groupby)):
+                    self.__liste_operations[i][1].col_x = table.col(self.__liste_operations[i][1].col_x)
+                    self.__liste_operations[i][1].col_y = table.col(self.__liste_operations[i][1].col_y)
+                    print(self.__liste_operations[i][1].__operation())
+                    dico_stat.update({i:self.__liste_operations[i][1].__operation()})
+                else:
+                    self.__liste_operations[i][1].col_x = self.__liste_operations[i][0].col(self.__liste_operations[i][1].col_x)
+                    self.__liste_operations[i][1].col_y = self.__liste_operations[i][0].col(self.__liste_operations[i][1].col_y)
+                    print(self.__liste_operations[i][1].__operation())
+                    dico_stat.update({i:self.__liste_operations[i][1].__operation()})
+            if any(isinstance(objet,Transformation) for objet in self.__liste_operations[i]):
+                try:
+                    if len(stockage_df) == 0:
+                        raise ValueError('vous n\'avez importer ou stocker aucun dataframe pour travailler dessus')
+                except ValueError as ve:
+                    print(ve)
+                    return ve
+                try:
+                    if all([self.__liste_operations[i][2].df_1 != df_stock.nom for df_stock in stockage_df]):
+                        raise IndexError('le dataframe renseigné n\'existe pas')
+                except IndexError as ie:
+                    print(ie)
+                    return ie
+                if isinstance(self.__liste_operations[i][2],Join):
+                    try:
+                        if all([self.__liste_operations[i][2].df_2 != df_stock.nom for df_stock in stockage_df]):
+                            raise IndexError('le dataframe pour la jointure renseigné n\'existe pas')
+                    except IndexError as ie:
+                        print(ie)
+                        return ie
+                if self.__liste_operations[i][0]:
+                    if isinstance(self.__liste_operations[i][2], Groupby):
+                        df_1 ,df_2 = self.__liste_operations[i][2]._operation()
+                        df_1.nom = self.__liste_operations[i][1]
+                        stockage_df.append(df_1)
+                        stockage_df.append(df_2)
+                    else:
+                        print(self.__liste_operations[i][2]._operation())
+                        df_1 = self.__liste_operations[i][2]._operation()
+                        df_1.nom = self.__liste_operations[i][1]
+                        stockage_df.append(df_1)
+                else:
+                    print(self.__liste_operations[i][1]._operation())
+            if any(isinstance(objet,import_export) for objet in self.__liste_operations[i]):
+                if isinstance(self.__liste_operations[i][1],my_import):
+                    header,data = self.__liste_operations[i][1]._importing()
+                    stockage_df.append(Dataframe(self.__liste_operations[i][0],header,data))
+                if isinstance(self.__liste_operations[i][1],my_export):
+                    try:
+                        if len(stockage_df) == 0:
+                            raise ValueError('vous n\'avez importer ou stocker aucun dataframe pour travailler dessus')
+                    except ValueError as ve:
+                        print(ve)
+                        return ve
+                    print(self.__liste_operations[i][1].exporting())
+            return stockage_df,dico_stat
