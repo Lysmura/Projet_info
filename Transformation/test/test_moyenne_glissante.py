e7b3da090320df466487e00c36face4e0feadc2d
Mohamed-Assili <105364732+meda6li@users.noreply.github.com>
2022-05-29 18:50:52 +0100

    Update test_moyenne_glissante.py

--- Transformation/test/test_moyenne_glissante.py
+++ Transformation/test/test_moyenne_glissante.py
@@ -4,10 +4,11 @@ from Structure.dataframe import Dataframe
 import unittest
 
 class Test_moyenne_glissante(unittest.TestCase):
-    header, data = import_json('Data/electricity_data','2013-01.json.gz').importing()
-    data_frame =Dataframe(nom="donneeselectr", header = header, data = data)
-    moy_gli = moyenne_glissante(data_frame,['consommation_brute_gaz_grtgaz'])._operation(3)
-    print(moy_gli)
+    def test_moyenne_glissante(self):
+        header, data = import_json('Data/electricity_data','2013-01.json.gz').importing()
+        data_frame =Dataframe(nom="donneeselectr", header = header, data = data)
+        moy_gli = moyenne_glissante(data_frame,['consommation_brute_gaz_grtgaz'])._operation(3)
+        print(moy_gli)
 
 if __name__ == '__main__':
     unittest.main()
