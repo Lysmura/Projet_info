d41b079bd863c8f8bb4150ba5db3861f39547170
ayoub1brm <105363414+ayoub1brm@users.noreply.github.com>
2022-05-29 21:21:08 +0200

    Update transformation_filter.py

--- Transformation/transformation_filter.py
+++ Transformation/transformation_filter.py
@@ -9,19 +9,19 @@ class Filter(Transformation):
             if critere not in ('==','<','>','>=','<='):
                 raise ValueError("l'argument critere doit appartenir à la liste suivante: ==,<,>,>=,<=")
         except ValueError as ve:
-            print(ve)
+            return ve
         self.__critere = critere
         try:
             if len(var) >1:
                 raise ValueError("var ne prend qu'une seule var en argument")
         except ValueError as ve:
-            print(ve)
+            return ve
 
         try:
             if isinstance(valeur,df.header[df.num_col(var[0])][1]) == False:
                 raise TypeError("la valeur du test n'est pas au même format que la variable")
         except TypeError as te:
-            print(te)
+            return te
         self.__valeur = valeur
 
     def _operation(self):
@@ -29,7 +29,7 @@ class Filter(Transformation):
             if isinstance(self.df_1.header[self.df_1.num_col(self.var[0])][1],str) and self.__critere != '==':
                 raise ValueError("le teste demander ne peut être effectué car la variable est du type chaine de caractere")
         except ValueError as ve:
-            print(ve)
+            return ve
 
         if self.__critere == '==':
             new_header = copy.deepcopy(self.df_1.header)
@@ -90,4 +90,3 @@ class Filter(Transformation):
                 else:
                     del trans_df.data[key]
             return trans_df
-        
