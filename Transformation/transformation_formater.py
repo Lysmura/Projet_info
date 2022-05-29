ac41f28430e04628a05a5c45323009328ec21fa1
ayoub1brm <105363414+ayoub1brm@users.noreply.github.com>
2022-05-29 22:58:03 +0200

    Update transformation_formater.py

--- Transformation/transformation_formater.py
+++ Transformation/transformation_formater.py
@@ -10,12 +10,12 @@ class Formater(Transformation):
 
     def _operation(self):
         try:
-            if isinstance(self.df_1.header[self.df_1.num_col(self.var)][1],self.__format_final):
+            if isinstance(self.df_1.header[self.df_1.num_col(self.var[0])],self.__format_final):
                 raise ValueError('La variable est déjà au format désiré')
         except ValueError as ve:
             return ve
         try:
-            if isinstance(self.df_1.header[self.df_1.num_col(self.var)][1],self.__format_depart):
+            if isinstance(self.df_1.header[self.df_1.num_col(self.var[0])],self.__format_depart):
                 raise ValueError('La variable format de depart indiqué est faux')
         except ValueError as ve:
             return ve
@@ -23,11 +23,11 @@ class Formater(Transformation):
         new_data = deepcopy(self.df_1.data)
         trans_df = Dataframe('formatage',new_header,new_data)
         for value in trans_df.values():
-            value[self.df_1.num_col(self.var)] =  self.__format_final(value[self.df_1.num_col(self.var)])
+            value[self.df_1.num_col(self.var[0])] =  self.__format_final(value[self.df_1.num_col(self.var[0])])
         if self.__format_final == str:
             for value in trans_df.values():
-                if str(value[self.df_1.num_col(self.var)])[-2:] == '.0':
-                    value[self.df_1.num_col(self.var)] =  str(value[self.df_1.num_col(self.var)]).replace('.0','')
+                if str(value[self.df_1.num_col(self.var[0])])[-2:] == '.0':
+                    value[self.df_1.num_col(self.var[0])] =  str(value[self.df_1.num_col(self.var[0])]).replace('.0','')
                 else:
-                    value[self.df_1.num_col(self.var)] = str(value[self.df_1.num_col(self.var)])
+                    value[self.df_1.num_col(self.var[0])] = str(value[self.df_1.num_col(self.var[0])])
         return trans_df
