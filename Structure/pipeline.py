39cf48a225916c91f4fcbd63825052865bcca85b
ayoub1brm <105363414+ayoub1brm@users.noreply.github.com>
2022-05-30 21:49:15 +0200

    Update pipeline.py

--- Structure/pipeline.py
+++ Structure/pipeline.py
@@ -21,7 +21,7 @@ class Pipeline:
         self.__op_possible = [Transformation,Univaries,Bivaries,import_export]
     
     def ajouter_operation(self,operation):
-        if if any([any([isinstance(element,op_possible) for element in operation]) for op_possible in self.__op_possible]):
+        if any([any([isinstance(element,op_possible) for element in operation]) for op_possible in self.__op_possible]):
             self.__liste_operations.append(operation)
         else:
             print('l\'operation n\'est pas reconnu et n\'a pas été ajouté pour execution')
@@ -33,8 +33,9 @@ class Pipeline:
     def execution(self):
         stockage_df = []
         dico_stat = {}
-        for i in range(len(self.__liste_operations)):
-            if isinstance(self.__liste_operations[i],Univaries):
+        for operation in self.__liste_operations:
+            print(operation)
+            if isinstance(operation,Univaries):
                 try:
                     if len(stockage_df) == 0:
                         raise ValueError('vous n\'avez importer ou stocker aucun dataframe pour travailler dessus')
@@ -42,35 +43,38 @@ class Pipeline:
                     print(ve)
                     return ve
                 try:
-                    if not isinstance(self.__liste_operations[i][0],Dataframe):
+                    if not isinstance(operation[0],Dataframe):
                         raise ValueError("pour une statistique descriptîve vous devez indiqué le dataframe en première position")
                 except ValueError as ve:
                     print(ve)
                     return ve
                 try:
-                    if self.__liste_operations[i][0] not in stockage_df:
+                    if operation[0] not in stockage_df:
                         raise IndexError('le dataframe renseigné n\'existe pas')
                 except IndexError as ie:
                     print(ie)
                     return ie
                 try:
-                    if all([self.__liste_operations[i][1].col != nom_colone[0] for nom_colone in self.__liste_operations[i][0].header]):
+                    if all([operation[1].col != nom_colone[0] for nom_colone in operation[0].header]):
                         raise IndexError("la variable {} est mal orthographié ou n'appartient pas aux dataframes indiqués".format(nom_var))
                 except IndexError as ie:
                     print(ie)
                     return ie
                 if  (self.__liste_operations[i-1][0] and isinstance(self.__liste_operations[i-1][2],Groupby)):
                     for table in stockage_df[-1]:
-                        self.__liste_operations[i][1].col = table.col(self.__liste_operations[i][1].col)
+                        operation[1].col = table.col(operation[1].col)
                         print('stat pour' +table.nom)
-                        print(self.__liste_operations[i][1].__operation())
-                        dico_stat.update({i:self.__liste_operations[i][1].__operation()})   
+                        print(operation[1].__operation())
+                        dico_stat.update({i:operation[1].__operation()})   
                 else:
-                    self.__liste_operations[i][1].col = self.__liste_operations[i][0].col(self.__liste_operations[i][1].col)
-                    print(self.__liste_operations[i][1].__operation())
-                    dico_stat.update({i:self.__liste_operations[i][1].__operation()})
-            
-            if isinstance(self.__liste_operations[i],Bivaries):
+                    for element in stockage_df:
+                        if  operation[0] == element.nom:
+                            operation[0] = element    
+                    operation[1].col = operation[0].col(operation[1].col)
+                    print(operation[1].__operation())
+                    dico_stat.update({i:operation[1].__operation()})
+
+            if isinstance(operation,Bivaries):
                 try:
                     if len(stockage_df) == 0:
                         raise ValueError('vous n\'avez importer ou stocker aucun dataframe pour travailler dessus')
@@ -78,26 +82,30 @@ class Pipeline:
                     print(ve)
                     return ve
                 try:
-                    if not isinstance(self.__liste_operations[i][0],Dataframe):
+                    if not isinstance(operation[0],Dataframe):
                         raise ValueError("pour une statistique descriptîve vous devez indiqué le dataframe en première position")
                 except ValueError as ve:
                     return ve
                 try:
-                    if all([self.__liste_operations[i][1].col_x != nom_colone[0] for nom_colone in self.__liste_operations[i][0].header]) or all([self.__liste_operations[i][1].col_y != nom_colone[0] for nom_colone in self.__liste_operations[i][0].header]):
+                    if all([operation[1].col_x != nom_colone[0] for nom_colone in operation[0].header]) or all([operation[1].col_y != nom_colone[0] for nom_colone in operation[0].header]):
                         raise IndexError("une des variables variable est mal orthographié ou n'appartient pas aux dataframes indiqués")
                 except IndexError as ie:
                     return ie
                 if (self.__liste_operations[i-1][0] and isinstance(self.__liste_operations[i-1][2],Groupby)):
-                    self.__liste_operations[i][1].col_x = table.col(self.__liste_operations[i][1].col_x)
-                    self.__liste_operations[i][1].col_y = table.col(self.__liste_operations[i][1].col_y)
-                    print(self.__liste_operations[i][1].__operation())
-                    dico_stat.update({i:self.__liste_operations[i][1].__operation()})
+                    for table in stockage_df[-1]:
+                        operation[1].col_x = table.col(operation[1].col_x)
+                        operation[1].col_y = table.col(operation[1].col_y)
+                        print(operation[1].__operation())
+                        dico_stat.update({i:operation[1].__operation()})
                 else:
-                    self.__liste_operations[i][1].col_x = self.__liste_operations[i][0].col(self.__liste_operations[i][1].col_x)
-                    self.__liste_operations[i][1].col_y = self.__liste_operations[i][0].col(self.__liste_operations[i][1].col_y)
-                    print(self.__liste_operations[i][1].__operation())
-                    dico_stat.update({i:self.__liste_operations[i][1].__operation()})
-            if any(isinstance(objet,Transformation) for objet in self.__liste_operations[i]):
+                    for element in stockage_df:
+                        if  operation[0] == element.nom:
+                            operation[0] = element
+                    operation[1].col_x = operation[0].col(operation[1].col_x)
+                    operation[1].col_y = operation[0].col(operation[1].col_y)
+                    print(operation[1].__operation())
+                    dico_stat.update({i:operation[1].__operation()})
+            if any(isinstance(objet,Transformation) for objet in operation):
                 try:
                     if len(stockage_df) == 0:
                         raise ValueError('vous n\'avez importer ou stocker aucun dataframe pour travailler dessus')
@@ -105,41 +113,66 @@ class Pipeline:
                     print(ve)
                     return ve
                 try:
-                    if all([self.__liste_operations[i][2].df_1 != df_stock.nom for df_stock in stockage_df]):
+                    if all([operation[2].df_1 != df_stock.nom for df_stock in stockage_df]):
                         raise IndexError('le dataframe renseigné n\'existe pas')
                 except IndexError as ie:
                     print(ie)
                     return ie
-                if isinstance(self.__liste_operations[i][2],Join):
+                if isinstance(operation[2],Join):
                     try:
-                        if all([self.__liste_operations[i][2].df_2 != df_stock.nom for df_stock in stockage_df]):
+                        if all([operation[2].df_2 != df_stock.nom for df_stock in stockage_df]):
                             raise IndexError('le dataframe pour la jointure renseigné n\'existe pas')
                     except IndexError as ie:
                         print(ie)
                         return ie
-                if self.__liste_operations[i][0]:
-                    if isinstance(self.__liste_operations[i][2], Groupby):
-                        df_1 ,df_2 = self.__liste_operations[i][2]._operation()
-                        df_1.nom = self.__liste_operations[i][1]
+                if operation[0]:
+                    if isinstance(operation[2], Groupby):
+                        for element in stockage_df:
+                            if  operation[2].df_1 == element.nom:
+                                operation[2].df_1 = element
+                        df_1 ,df_2 = operation[2]._operation()
+                        df_1.nom = operation[1]
                         stockage_df.append(df_1)
+                        print(df_1)
                         stockage_df.append(df_2)
                     else:
-                        print(self.__liste_operations[i][2]._operation())
-                        df_1 = self.__liste_operations[i][2]._operation()
-                        df_1.nom = self.__liste_operations[i][1]
+                        for element in stockage_df:
+                            if  operation[2].df_1 == element.nom:
+                                operation[2].df_1 = element
+                        if operation[2].df_2 != None:
+                            for element in stockage_df:
+                                if  operation[2].df_2 == element.nom:
+                                    operation[2].df_2 = element
+                        print(operation[2]._operation())
+                        df_1 = operation[2]._operation()
+                        df_1.nom = operation[1]
                         stockage_df.append(df_1)
                 else:
-                    print(self.__liste_operations[i][1]._operation())
-            if any(isinstance(objet,import_export) for objet in self.__liste_operations[i]):
-                if isinstance(self.__liste_operations[i][1],my_import):
-                    header,data = self.__liste_operations[i][1]._importing()
-                    stockage_df.append(Dataframe(self.__liste_operations[i][0],header,data))
-                if isinstance(self.__liste_operations[i][1],my_export):
+                    for element in stockage_df:
+                            if  operation[1].df_1 == element.nom:
+                                operation[1].df_1 = element
+                    if operation[1].df_2 != None:
+                        for element in stockage_df:
+                            if  operation[1].df_2 == element.nom:
+                                operation[1].df_2 = element
+                    
+                    print(operation[1]._operation())
+            if any(isinstance(objet,import_export) for objet in operation):
+                if isinstance(operation[1],my_import):
+                    header,data = operation[1].importing()
+                    stockage_df.append(Dataframe(operation[0],header,data))
+                    print(Dataframe(operation[0],header,data))
+                if isinstance(operation[1],my_export):
                     try:
                         if len(stockage_df) == 0:
                             raise ValueError('vous n\'avez importer ou stocker aucun dataframe pour travailler dessus')
                     except ValueError as ve:
                         print(ve)
                         return ve
-                    print(self.__liste_operations[i][1].exporting())
-            return stockage_df,dico_stat
+                    for element in stockage_df:
+                        if  operation[1].dataframe == element.nom:
+                            operation[1].dataframe = element
+                                    
+                    print(operation[1].exporting())
+        self.__liste_operations = []
+        return stockage_df,dico_stat
